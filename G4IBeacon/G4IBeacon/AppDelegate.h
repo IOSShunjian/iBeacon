d4e109b25b278b5979314968b38d2d7cea270319
LiuHuiYang <liuhuiyang2009@163.com>
2017-05-05 11:58:50 +0800

    创建项目

---
+++ G4IBeacon/G4IBeacon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  G4IBeacon
+//
+//  Created by LHY on 2017/5/5.
+//  Copyright © 2017年 SmartHomeGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
