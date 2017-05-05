1b69a41b04046d593368a0bbd370be8d3b1eaeb0
LiuHuiYang <liuhuiyang2009@163.com>
2017-05-05 13:30:56 +0800

    自定义基类

---
+++ G4IBeacon/G4IBeacon/Classes/Base/Controller/SHNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  SHNavigationController.h
+//  G4IBeacon
+//
+//  Created by LHY on 2017/5/5.
+//  Copyright © 2017年 SmartHomeGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHNavigationController : UINavigationController
+
+@end
---
+++ G4IBeacon/G4IBeacon/Classes/Base/Controller/SHTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SHTabBarController.h
+//  G4IBeacon
+//
+//  Created by LHY on 2017/5/5.
+//  Copyright © 2017年 SmartHomeGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHTabBarController : UITabBarController
+
+@end
