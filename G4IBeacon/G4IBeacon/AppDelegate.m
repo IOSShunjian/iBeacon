1f78442c29fcc006fd64a9cb8f83f8c8d2553687
LiuHuiYang <liuhuiyang2009@163.com>
2017-05-12 11:57:19 +0800

    完善后台执行

--- G4IBeacon/G4IBeacon/AppDelegate.m
+++ G4IBeacon/G4IBeacon/AppDelegate.m
@@ -58,12 +58,14 @@
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+   
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+//    SHLog(@"回到前台");
+    [[UIApplication sharedApplication] endBackgroundTask:self.task];
 }
 
 
